e119f0511ebe700fc647ee3c983c3d446fd4006f
info@whykiki.de <421bbdc0ad+>
2021-07-14 13:45:35 +0200

    - Start UMbau Jobdetails

--- wp-content/plugins/bms_prescreen/classes/class-jobdetails.php
+++ wp-content/plugins/bms_prescreen/classes/class-jobdetails.php
@@ -34,11 +34,49 @@ class Jobdetails{
         ];
 
         $response = $this->apiHelper->PrescreenAPI('job', 'GET', $parameters);
+
+        //Post mit ID Startseite holen --> Startseite
+        // alles soweit entfernen und das YOOTheme JSON holen
+
+        $startseitenPost = get_post( 2 );
+        $companyGridLength = strlen($startseitenPost->post_content);
+        $companyGridStart = stripos ( $startseitenPost->post_content, '<!--more-->' );
+        $companyGrid = substr( $startseitenPost->post_content, $companyGridStart + 11, $companyGridLength);
+        $companyGrid = substr( $companyGrid, 5, strlen($companyGrid));
+        $companyGrid = substr( $companyGrid, 0, -3);
+        $companyGrid = json_decode( $companyGrid, false );
+
+        //Company Grid Item
+        // achtung --> 5 scheint ROW zu sein, bei Umbau der Startseite muss ggf hier angepasst werden
+
+        $numOfItems = count($companyGrid->children[5]->children[0]->children[1]->children[1]->children);
+
+        $companyInformations = array();
+        for($i = 0; $i < $numOfItems; $i++){
+            $item = $companyGrid->children[5]->children[0]->children[1]->children[1]->children[$i]->props;
+            $company        = $item->title;
+            $webseiteLink   = $item->webseiteLink;
+            $jobLink        = $item->jobLink;
+            $logo           = $item->image;
+            $headline       = $item->headline;
+            $content        = $item->content;
+
+            $companyInformations[$company] = [
+                'company'       =>  $company,
+                'content'       =>  $content,
+                'webseiteLink'  =>  $webseiteLink,
+                'jobLink'       =>  $jobLink,
+                'logo'          =>  $logo,
+                'headline'      =>  $headline
+            ];
+        }
+
         if ( $id ) {
             $args = [
                 'id'        =>  $id,
                 'response'  =>  $response,
-                'slug'      =>  $slug
+                'slug'      =>  $slug,
+                'company'   =>  $companyInformations
             ];
             load_template(  BMSPRE_PLUGIN_DIR . '/templates/job-detail.php',  $require_once = true,  $args);
             exit;
